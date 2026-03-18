1b6b70ff6a738cce18b77d04eb038b190811883a
compasshousingsociety-eng <compasshousingsociety@gmail.com>
2026-03-18 12:17:31 +0530

    Add files via upload

---
+++ footer.js
@@ -0,0 +1,88 @@
+const footer = `
+<footer>
+
+    <!-- FOOTER STATS BAR -->
+    <section class="footer-stats">
+        <div class="stat">
+            <h3>34K+</h3>
+            <p>Success Stories</p>
+        </div>
+        <span class="stat-divider"></span>
+
+        <div class="stat">
+            <h3>210+</h3>
+            <p>Professional Team</p>
+        </div>
+        <span class="stat-divider"></span>
+
+        <div class="stat">
+            <h3>54K+</h3>
+            <p>Branches Nationwide</p>
+        </div>
+        <span class="stat-divider"></span>
+
+        <div class="stat">
+            <h3>35K+</h3>
+            <p>Worldwide Clients</p>
+        </div>
+    </section>
+
+    <!-- FOOTER TOP -->
+    <section class="footer-top">
+
+        <div class="footer-brand">
+            <h4>Compass Housing Society</h4>
+            <p>
+                We’re a team of strategic real estate experts delivering
+                premium homes and investment opportunities designed
+                for long-term value and lifestyle excellence.
+            </p>
+           
+        </div>
+
+        <nav>
+            <h4>Company</h4>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Events</a></li>
+            </ul>
+        </nav>
+
+        <nav>
+            <h4>Help</h4>
+            <ul>
+                <li><a href="#">Customer Support</a></li>
+                <li><a href="#">How It Works</a></li>
+                <li><a href="#">Terms & Conditions</a></li>
+                <li><a href="#">Privacy Policy</a></li>
+            </ul>
+        </nav>
+
+        <div>
+            <h4>Work Hours</h4>
+            <ul class="footer-contact">
+                <li>🕘 9 AM – 5 PM, Monday – Saturday</li>
+                <li>✉️ support@compasshousing.com</li>
+                <li>📍 Hyderabad, India – 500084</li>
+                <li>📞 +91 98765 43210</li>
+                
+            </ul>
+        </div>
+
+    </section>
+
+    <!-- FOOTER BOTTOM -->
+    <section class="footer-bottom">
+      
+    </section>
+
+</footer>
+`;
+
+document.getElementById("side-footer").innerHTML = footer;
+
+
+
+
